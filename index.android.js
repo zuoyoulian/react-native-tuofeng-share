81f794fb1827e6b13a6af83a028848dbc8e61596
荆尧坤 <jingyaokun@tuofeng.cn>
2017-05-16 10:34:33 +0800

    格式整理(部分)

--- index.android.js
+++ index.android.js
@@ -1,9 +1,6 @@
-'use strict'
-import {
-  NativeModules,DeviceEventEmitter
-} from 'react-native';
+import { NativeModules } from 'react-native'
 
-var ShareMsg = NativeModules.ShareMsg
+const ShareMsg = NativeModules.ShareMsg
 // var wechat_id = 'wx520a68af75ac86f2'
 // var qq_id = '1104812217'
 // var weibo_id = '2038575089'
@@ -23,99 +20,99 @@ export default class Share {
       })
   }
    */
-  //android接口初始化 
-  //微信
-  initWechat(wechat_id){
-    ShareMsg.initWechat(wechat_id)
+  // android 接口初始化
+  // 微信
+  static initWechat(wechatId) {
+    ShareMsg.initWechat(wechatId)
   }
-  //QQ
-  initQQ(qq_id){
-    ShareMsg.initQQ(qq_id)
+  // QQ
+  static initQQ(qqId) {
+    ShareMsg.initQQ(qqId)
   }
-  //微博
-  initWeibo(weibo_id){
-    ShareMsg.initWeibo(weibo_id)
+  // 微博
+  static initWeibo(weiboId) {
+    ShareMsg.initWeibo(weiboId)
   }
 
   // 判断是否安装QQ
-  IsQQInstalled() {
-    return new Promise(function(resolve, reject){
-      ShareMsg.isInstallQQ('com.tencent.mobileqq',cb=>{
+  static IsQQInstalled() {
+    return new Promise((resolve) => {
+      ShareMsg.isInstallQQ('com.tencent.mobileqq', (cb) => {
         resolve(cb)
       })
     })
   }
   // 判断是否安装微信
-  IsWeixinInstalled() {
-    return new Promise(function(resolve, reject){
-      ShareMsg.isInstallWechat('com.tencent.mm',cb=>{
+  static IsWeixinInstalled() {
+    return new Promise((resolve) => {
+      ShareMsg.isInstallWechat('com.tencent.mm', (cb) => {
         resolve(cb)
       })
     })
   }
   // 判断是否安装微博
-  IsWeiboInstalled() {
-    return new Promise(function(resolve, reject){
-      ShareMsg.isInstallWeibo('com.sina.weibo',cb=>{
+  static IsWeiboInstalled() {
+    return new Promise((resolve) => {
+      ShareMsg.isInstallWeibo('com.sina.weibo', (cb) => {
         resolve(cb)
       })
     })
   }
 
   // 授权登录
-  weixinLogin(weiXinAppID) {
-    ShareMsg.loginWechat('','')
+  static weixinLogin() {
+    ShareMsg.loginWechat('', '')
   }
 
-  qqLogin(QQAppID) {
+  static qqLogin() {
     ShareMsg.loginQQ('')
   }
 
-  weiboLogin(weiboAppKey) {
-    ShareMsg.loginWeibo(weiboAppKey,'http://tuofeng.cn','')
+  static weiboLogin(weiboAppKey) {
+    ShareMsg.loginWeibo(weiboAppKey, 'http://tuofeng.cn', '')
   }
 
-// 分享  标题、描述、图片、链接
+  // 分享  标题、描述、图片、链接
 
-// 分享到微博
-/**
-  参数：info.type 0:图文链接；1:本地图片
-  info  分享的内容  例如：{title: '测试', desc: 'rn分享', logo: 'http://jijia.tuofeng.cn/plan/images/ins_company/ddhrs.jpg', url: 'http://jijia.tuofeng.cn/#plans'}
-**/
-shareToWeibo(info, weiboAppKey) {
-  switch(info.type){
+  // 分享到微博
+  /**
+    参数：info.type 0:图文链接；1:本地图片
+    info  分享的内容  例如：{title: '测试', desc: 'rn分享', logo: 'http://jijia.tuofeng.cn/plan/images/ins_company/ddhrs.jpg', url: 'http://jijia.tuofeng.cn/#plans'}
+  **/
+  static shareToWeibo(info) {
+    switch (info.type) {
     case 1:
-      ShareMsg.shareWeibo(1,'这款产品真好',info.title,info.desc,info.imagePath,info.url)
+      ShareMsg.shareWeibo(1, '这款产品真好', info.title, info.desc, info.imagePath, info.url)
       break
     default:
-      ShareMsg.shareWeibo(0,'这款产品真好',info.title,info.desc,info.logo,info.url)
-  }
-}
-
-// 分享到QQ
-/**
-  参数：
-  info.type 1:图文链接；5:本地图片
-  info  分享的内容  例如：{title: '测试', desc: 'rn分享', logo: 'http://jijia.tuofeng.cn/plan/images/ins_company/ddhrs.jpg', url: 'http://jijia.tuofeng.cn/#plans'}
-  shareTo  类型  0 QQ好友  1 QQ空间  8 QQ收藏   16 QQ数据线 电脑共享
-**/
-  shareToQQ(info, shareTo, QQAppID) {
-    switch(info.type){
-    case 5:
-      ShareMsg.shareQQ(5,info.title,info.desc,info.imagePath,info.url)
-      break
-    default:
-      ShareMsg.shareQQ(1,info.title,info.desc,info.logo,info.url)
+      ShareMsg.shareWeibo(0, '这款产品真好', info.title, info.desc, info.logo, info.url)
     }
   }
 
-// 分享到微信
-/**
-  参数：
-  info 分享的内容  例如：{title: '测试', desc: 'rn分享', logo: 'http://jijia.tuofeng.cn/plan/images/ins_company/ddhrs.jpg', url: 'http://jijia.tuofeng.cn/#plans'}
-  shareTo  分享的类型   0 微信好友  1 微信朋友圈   2 微信收藏
-**/
-  shareToWeixin(info, weiXinAppID, shareTo, weiXindict) {
+  // 分享到QQ
+  /**
+    参数：
+    info.type 1:图文链接；5:本地图片
+    info  分享的内容  例如：{title: '测试', desc: 'rn分享', logo: 'http://jijia.tuofeng.cn/plan/images/ins_company/ddhrs.jpg', url: 'http://jijia.tuofeng.cn/#plans'}
+    shareTo  类型  0 QQ好友  1 QQ空间  8 QQ收藏   16 QQ数据线 电脑共享
+  **/
+  static shareToQQ(info) {
+    switch (info.type) {
+    case 5:
+      ShareMsg.shareQQ(5, info.title, info.desc, info.imagePath, info.url)
+      break
+    default:
+      ShareMsg.shareQQ(1, info.title, info.desc, info.logo, info.url)
+    }
+  }
+
+  // 分享到微信
+  /**
+    参数：
+    info 分享的内容  例如：{title: '测试', desc: 'rn分享', logo: 'http://jijia.tuofeng.cn/plan/images/ins_company/ddhrs.jpg', url: 'http://jijia.tuofeng.cn/#plans'}
+    shareTo  分享的类型   0 微信好友  1 微信朋友圈   2 微信收藏
+  **/
+  static shareToWeixin(info, weiXinAppID, shareTo) {
     const shareOptions = {
       type: info.type ? info.type : 3,
       webpageUrl: info.url,
@@ -130,7 +127,7 @@ shareToWeibo(info, weiboAppKey) {
     ShareMsg.shareWechat(shareOptions)
   }
 
-  shareToWeixinMini(info, weiXinAppID, shareTo, weiXindict) {
+  static shareToWeixinMini(info, weiXinAppID, shareTo) {
     ShareMsg.initWechat(weiXinAppID)
     const shareOptions = {
       type: 6,
@@ -147,7 +144,7 @@ shareToWeibo(info, weiboAppKey) {
   }
 
 
- //发送邮件
+  // 发送邮件
    /**
    参数 info:
        {
@@ -158,13 +155,13 @@ shareToWeibo(info, weiboAppKey) {
        "body": ""  // 邮件正文
        }
    **/
-   shareToMail(info, callBack) {
-     //仅支持首个地址
- 	  //参数信息 目标地址,标题,内容
-    ShareMsg.shareEmail(info.toRecipients[0],info.title,info.body)
-   }
+  static shareToMail(info) {
+    // 仅支持首个地址
+    // 参数信息 目标地址,标题,内容
+    ShareMsg.shareEmail(info.toRecipients[0], info.title, info.body)
+  }
 
-   //发送短信
+  // 发送短信
    /**
    参数info：
        {
@@ -172,11 +169,10 @@ shareToWeibo(info, weiboAppKey) {
        "body": ""  // 短信正文
        }
    **/
-   shareToMessage(info, callBack) {
-     //仅支持首个电话
- 	  //参数信息 目标电话,短信文本,附加链接地址
-    ShareMsg.shareSMS(info.phones[0],info.body)
-   }
+  static shareToMessage(info) {
+    // 仅支持首个电话
+    // 参数信息 目标电话,短信文本,附加链接地址
+    ShareMsg.shareSMS(info.phones[0], info.body)
+  }
 
 }
-module.exports = new Share();
