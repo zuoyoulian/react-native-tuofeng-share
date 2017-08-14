886d61b560e2ea02a3c80cc5c8de7738efe4f1bb
荆尧坤 <jingyaokun@tuofeng.cn>
2017-08-14 11:07:30 +0800

    适配rn0.47

--- android/src/main/java/com/sharemsg/ShareMsgPackage.java
+++ android/src/main/java/com/sharemsg/ShareMsgPackage.java
@@ -22,7 +22,6 @@ public class ShareMsgPackage implements ReactPackage {
         return modules;
     }
 
-    @Override
     public List<Class<? extends JavaScriptModule>> createJSModules() {
         return Collections.emptyList();
     }
